c1743ac693a6cd8582a82797d9a91877e9fb2f87
hshziwo <hshziwo@gmail.com>
2023-12-29 19:47:47 +0900

    Flask + React 기본환경 세팅

---
+++ front_app/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/flask/hello')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log("SUCCESS", data)
+      setGetMessage(data)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.resultStatus === 'SUCCESS' ? 
+          <h3>{getMessage.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
