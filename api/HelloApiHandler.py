9e6fabd2ef6661b097ef69458d5bb376d13b1bd0
hshziwo <hshziwo@gmail.com>
2023-12-30 16:39:07 +0900

    konlpy 테스트

--- api/HelloApiHandler.py
+++ api/HelloApiHandler.py
@@ -1,11 +1,56 @@
 from flask_restful import Api, Resource, reqparse
+import requests
+import config
+import json
+from konlpy.tag import Okt
 
 class HelloApiHandler(Resource):
+  # def get(self):
+  #   return {
+  #     'resultStatus': 'SUCCESS',
+  #     'message': "Hello Api Handler"
+  #     }
+
   def get(self):
+    with open('testdata.json') as f:
+      json_object = json.load(f)
+
+    okt = Okt()
+
     return {
       'resultStatus': 'SUCCESS',
-      'message': "Hello Api Handler"
+      'message': "Hello Api Handler",
+      'test': okt.nouns(json_object['items'][0]['description'])
       }
+
+  # def get(self):
+  #   # 뉴스 검색 API의 http 메소드가 get이므로 get 사용
+  #   # 네이버 뉴스 API에 안내사항 따라함
+  #   keyword = "기안84"
+
+  #   response = requests.get(
+  #       'https://openapi.naver.com/v1/search/news.json',
+  #       params = {
+  #           'query' : keyword,
+  #           'display' : 100,
+  #           'sort' : 'sim' # 'sim' | 'date'
+  #       },
+  #       headers= {
+  #           'X-Naver-Client-Id' : config.NAVER_CLIENT_ID,
+  #           'X-Naver-Client-Secret' : config.NAVER_CLIENT_SECRET
+  #       }
+  #   )
+
+  #   # print(response.json())
+
+  #   # 우리 서버의 API에 맞게 데이터 가공
+  #   items = response.json()
+
+  #   return {
+  #     'resultStatus': 'SUCCESS',
+  #     'message': "Hello Api Handler",
+  #     'items' : items
+  #     }
 
   def post(self):
     print(self)
