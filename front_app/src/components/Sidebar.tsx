feac0201bff10778579b03e6aad4377d43a51730
hshziwo <hshziwo@gmail.com>
2024-01-01 12:25:43 +0900

    사이드바 구현

---
+++ front_app/src/components/Sidebar.tsx
@@ -0,0 +1,138 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { FcComboChart } from 'react-icons/fc';
+
+import { SidebarData } from './SidebarData';
+
+const BIIcon = styled.div`
+    /* display: flex;
+    justify-content: start;
+    align-items: center;
+    height: 3.5rem;
+    font-size: 1.5rem;
+    margin-left: 2rem; */
+
+    display: flex;
+    height: 4rem;
+    align-items: center;
+    padding: 0.5rem;
+    padding-bottom: 1.5rem;
+    font-size: 3.5rem;
+`;
+
+const SidebarMenu = styled.div`
+    /* width: 220px;
+    height: 100vh;
+    background-color: rgb(255 255 255 / 1);
+    position: fixed;
+    top: 0;
+    left: 0; */
+
+    box-sizing: border-box;
+    border-style: solid;
+    position: fixed;
+    top: 0px;
+    bottom: 0px;
+    z-index: 50;
+    display: flex;
+    width: 16rem;
+    flex-direction: column;
+    overflow-y: auto;
+    border-right-width: 1px;
+    border-color: rgb(229 231 235 / 1);
+    background-color: rgb(255 255 255 / 1);
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    padding-bottom: 1rem;
+`;
+
+const MenuItems = styled.ul`
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    width: 100%;
+    height: 50px;
+    /* padding: 1rem 0 1.25rem; */
+
+    /* tab-size: 4; */
+    /* font-variation-settings: normal; */
+    /* line-height: inherit; */
+    /* box-sizing: border-box; */
+    /* border-width: 0; */
+    /* border-style: solid; */
+    /* border-color: #e5e7eb; */
+    /* list-style: none; */
+    margin: 0;
+    padding: 0;
+    /* margin-left: -0.5rem; */
+    /* margin-right: -0.5rem; */
+`;
+
+// const MenuItemLinks = styled(Link)`
+const MenuItemLinks = styled.li`
+    /* display: flex; */
+    align-items: center;
+    /* padding: 0 2rem; */
+    /* font-size: 20px; */
+    /* text-decoration: none; */
+    /* color: #ffffff; */
+
+    /* tab-size: 4; */
+    /* font-variation-settings: normal; */
+    /* box-sizing: border-box; */
+    border-width: 0;
+    /* border-style: solid; */
+    /* border-color: #e5e7eb; */
+    /* list-style: none; */
+    /* text-decoration: inherit; */
+    display: flex;
+    column-gap: 0.75rem;
+    padding: 0.5rem;
+    font-size: 0.875rem;
+    font-weight: 600;
+    line-height: 1.5rem;
+    color: rgb(55 65 81 / 1);
+
+    &:hover {
+        /* background-color: #ffffff; */
+        /* color: #000080; */
+        width: 100%;
+        height: 45px;
+        /* text-align: center; */
+        border-radius: 5px;
+        /* margin: 0 2rem; */
+        color: rgb(79 70 229 / 1);
+        background-color: rgb(240 240 241 / 1);
+    }
+`;
+
+const MenuIcon = styled.div`
+    font-size: 1.5rem;
+`;
+
+const Sidebar = () => {
+    return (
+        <>
+            <SidebarMenu>
+                <BIIcon>
+                    <FcComboChart />
+                </BIIcon>
+
+                {SidebarData.map((item, index) => {
+                    return (
+                        <MenuItems>
+                            {/* <MenuItemLinks to={item.path}> */}
+                            <MenuItemLinks>
+                                <MenuIcon>{item.icon}</MenuIcon>
+                                {item.title}
+                            </MenuItemLinks>
+                        </MenuItems>
+                    );
+                })}
+            </SidebarMenu>
+        </>
+    );
+};
+
+export default Sidebar;
---
+++ front_app/src/components/SidebarData.tsx
@@ -0,0 +1,21 @@
+import { MdOutlineDescription } from 'react-icons/md';
+import { AiOutlineHome } from 'react-icons/ai';
+import { IoBarChartOutline } from 'react-icons/io5';
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiOutlineHome />,
+    },
+    {
+        title: 'Description',
+        path: '/',
+        icon: <MdOutlineDescription />,
+    },
+    {
+        title: 'Analytics',
+        path: '/',
+        icon: <IoBarChartOutline />,
+    },
+];
