14ebfaa106109782cc09438ae4b0009d0a5498e5
hshziwo <hshziwo@gmail.com>
2024-01-10 18:30:02 +0900

    redirect 테스트 빌드 커밋

--- front_app/config-overrides.js
+++ front_app/config-overrides.js
@@ -1,22 +1,7 @@
-// module.exports = {
-//     webpack: function (config, env) {
-//         config.output.filename = 'static/js/[name].[hash:8].js';
-//         config.output.chunkFilename = 'static/js/[name].[hash:8].chunk.js';
-//         return config;
-//     },
-// };
-
 module.exports = {
     webpack: function (config, env) {
-        return {
-            ...config,
-            output: {
-                filename: 'static/js/[name].[hash:8].js',
-                chunkFilename: 'static/js/[name].[hash:8].chunk.js',
-            },
-            devServer: {
-                historyApiFallback: true,
-            },
-        };
+        config.output.filename = 'static/js/[name].[hash:8].js';
+        config.output.chunkFilename = 'static/js/[name].[hash:8].chunk.js';
+        return config;
     },
 };
