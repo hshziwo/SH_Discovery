27056cb4f5a0b681390aabd59708b20532588e32
hshziwo <hshziwo@gmail.com>
2024-01-10 18:28:37 +0900

    임시 에러처리

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, redirect
 from flask_cors import CORS #comment this on deployment
 from api.api import addApi
 
@@ -7,5 +7,9 @@ CORS(app) #comment this on deployment
 addApi(app) #개발과 배포 빌드 app.py의 차이로 인해 하위 함수로 변경
 
 @app.route("/", defaults={'path':''})
-def serve(path):
+def index(path):
     return send_from_directory(app.static_folder,'index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return redirect('/'), 404
