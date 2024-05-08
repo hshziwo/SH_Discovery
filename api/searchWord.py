ffb1b2c21da80386b937b4e048e9f22e5d9aaccf
hshziwo <hshziwo@gmail.com>
2024-05-08 12:25:00 +0900

    koyeb 메모리 이슈로 파라미터 수정

--- api/searchWord.py
+++ api/searchWord.py
@@ -1,18 +1,19 @@
 import requests
 import config
 
+
 def searchWord(keyword):
     response = requests.get(
-        'https://openapi.naver.com/v1/search/news.json',
-        params = {
-            'query' : keyword,
-            'display' : 100,
-            'sort' : 'sim' # 'sim' | 'date'
+        "https://openapi.naver.com/v1/search/news.json",
+        params={
+            "query": keyword,
+            "display": 10,  # 100, # koyeb 무료 배포 메모리 부족으로 데이터 줄임.
+            "sort": "sim",  # 'sim' | 'date'
         },
-        headers= {
-            'X-Naver-Client-Id' : config.NAVER_CLIENT_ID,
-            'X-Naver-Client-Secret' : config.NAVER_CLIENT_SECRET
-        }
+        headers={
+            "X-Naver-Client-Id": config.NAVER_CLIENT_ID,
+            "X-Naver-Client-Secret": config.NAVER_CLIENT_SECRET,
+        },
     )
 
     # print(response.json())
